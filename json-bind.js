c33b4819f745e89f4cc34ac83ad361fa06aa0741
chrisclark <chris@untrod.com>
2013-07-03 13:02:20 -0700

    Clean up via jslint

--- json-bind.js
+++ json-bind.js
@@ -1,30 +1,44 @@
-(function($) {
-    $.fn.BindJson = function(fielddata, cssClassPrefix) {
-        var selectorPrefix = " ." + cssClassPrefix;
-        for (key in fielddata) {
-            var ctl = $(selectorPrefix + key, this);
-            if (ctl.length != 0) {
-                var isTextField = (ctl[0].tagName == "DIV" || ctl[0].tagName == "SPAN" || ctl[0].tagName == "LABEL");
-                if (isTextField) ctl.text(fielddata[key]);
-                if (ctl[0].type == 'checkbox') ctl.attr('checked', (fielddata[key] == true));
-                else ctl.val(fielddata[key]);
+(function ($) {
+    $.fn.BindJson = function (fieldData, cssClassPrefix) {
+        var selectorPrefix = " ." + cssClassPrefix,
+            key = [],
+            ctl = null,
+            isTextField = false,
+            fill = "";
+        for (key in fieldData) {
+            if (fieldData.hasOwnProperty(key)) {
+                ctl = $(selectorPrefix + key, this);
+                fill = fieldData[key];
+                if (ctl.length != 0) {
+                    isTextField = (ctl[0].tagName == "DIV" || ctl[0].tagName == "SPAN" || ctl[0].tagName == "LABEL");
+                    if (isTextField) {
+                        ctl.text(fill);
+                    } else if (ctl[0].type == 'checkbox') {
+                        ctl.attr('checked', (fill == true));
+                    } else {
+                        ctl.val(fill);
+                    }
+                }
             }
         }
     };
-
 })(jQuery);
 
-(function($) {
-    $.fn.ClearDataFields = function(clearClass) {
-        $(":input", this).each(function() {
-            var type = this.type;
-            var tag = this.tagName.toLowerCase();
-            if (type == 'text' || type == 'password' || tag == 'textarea' || type == 'hidden') this.value = "";
-            else if (type == 'checkbox' || type == 'radio') this.checked = false;
-            else if (tag == 'select') this.selectedIndex = 0;
+(function ($) {
+    $.fn.ClearDataFields = function (clearClass) {
+        $(":input", this).each(function () {
+            var type = this.type,
+                tag = this.tagName.toLowerCase();
+            if (type == 'text' || type == 'password' || tag == 'textarea' || type == 'hidden') {
+                this.value = "";
+            } else if (type == 'checkbox' || type == 'radio') {
+                this.checked = false;
+            } else if (tag == 'select') {
+                this.selectedIndex = 0;
+            }
         });
-        //clearClass needed to clear non-input elements
-        $("." + clearClass, this).text("");
+        if (clearClass) {
+            $("." + clearClass, this).text("");
+        }
     };
 })(jQuery);
-});
